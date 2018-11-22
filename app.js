f593bf59674b79e22131ad1201c6c107cff4bfa5
Illuminatiiiiii <kinglydeveloper@gmail.com>
2018-11-21 21:43:42 -0600

    Adding Games/Using SWFObject

--- app.js
+++ app.js
@@ -1,70 +1,71 @@
 const express = require("express");
 const app = express();
 const request = require("request");
-
+ 
 //Sets the public folder as the external file folder
 app.use(express.static("public"));
-
+ 
 //Officially sets the view engine as ejs, therefore setting the default file type for readering to .ejs
 app.set("view engine", "ejs");
-
-app.get("/", function (req, res) {
-    res.render("homepage");
+ 
+app.get("/", function(req, res){
+    res.render("homepage"); 
 });
-
-app.get("/game/:gameTitle/:gameCreator", function (req, res) {
-    const title = req.params.gameTitle;
-    const creator = req.params.gameCreator;
+ 
+app.get("/game/:title/:creator/:width/:height/:fileName/:thumbnailFile", function(req, res){
     res.render("game", {
-        title: title,
-        creator: creator
+        title: req.params.title,
+        creator: req.params.creator,
+        width: req.params.width,
+        height: req.params.height,
+        fileName: req.params.fileName,
+        thumbnailFile: req.params.thumbnailFile
     });
 });
-
-app.get("/list", function (req, res) {
-
-    const games = [{
-            title: "Fortnite",
-            creator: "Epic Games"
+ 
+app.get("/list", function(req, res){ 
+ 
+    //Our ghetto database
+    const gamesData = [
+        {
+            title: "American Racing", 
+            creator: "turboNuke",
+            width: 640,
+            height: 480,
+            fileName: "americanracing.swf",
+            thumbnailFile: "americanracingpicture.jpg"
         },
         {
-            title: "Dirty Bomb",
-            creator: "Splash Damage"
+            title: "Generic Defense Game", 
+            creator: "PyschoGoldfish",
+            width: 640,
+            height: 480,
+            fileName: "genericdefense.swf",
+            thumbnailFile: "GenericDefenseGame.png"
         },
         {
-            title: "Battlefield 1",
-            creator: "EA"
+            title: "Learn to Fly 2", 
+            creator: "light_bringer777",
+            width: 640,
+            height: 480,
+            fileName: "embeddable_115608.swf",
+            thumbnailFile: "ltf2.jpg"
+        },
+        {
+            title: "Wonderputt", 
+            creator: "dampgnat",
+            width: 750,
+            height: 650,
+            fileName: "wonderputt.swf",
+            thumbnailFile: "pop-wonderputt.jpg"
         }
     ]
-
+ 
     res.render("list", {
-        gamesList: games
+        gamesData: gamesData
     });
 });
-
-app.get("/pics", function (req, res) {
-    var searchTerm = req.query.searchterm;
-    var page = req.query.page;
-    console.log(searchTerm);
-    request("https://api.unsplash.com/search/photos?client_id=a23535d66eec2d81f1d9aea445095e620bf47ec2df2b80266d2b7b92adf2d844&page=" + page + "&query=" + searchTerm, function (error, response, body) {
-        if (error) {
-            console.log(error);
-        } else {
-            var data = JSON.parse(body);
-            res.render("pictures", {
-                picData: data,
-                pageNumber: page
-            });
-        }
-    });
-});
-
-//Route to page so user can search for a picture
-app.get("/search", function(req, res){
-    res.render("search");
-});
-
-
-app.listen("3000", function () {
+ 
+app.listen("3000", function(){
     console.log("Gaming Website has started up! Made by Illuminati Productions.");
 });
